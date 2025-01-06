d548cbf09b17018e8656f43c0d4044f1199fa91d
Nityom Tikhe <nityomtikherr@gmail.com>
2025-01-06 17:14:35 +0530

    Implement connection request functionality; add new connectionRequest model and update request routes to handle sending requests with status

--- devTinder-backend/src/routes/request.js
+++ devTinder-backend/src/routes/request.js
@@ -1,13 +1,62 @@
-
 const express = require('express');
 const requestRouter = express.Router();
-const {userAuth} = require("../middlewares/auth");
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require('../models/user');
 
-requestRouter.post("/sendConnectionRequest",userAuth,async(req,res)=>{
-    const user = req.user;
-      //send connection request
-      console.log("connection request sent");
-      res.send(user.firstName + " send the connection request");
+// Send connection request
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+  try {
+    const fromUserId = req.user._id; // Authenticated user's ID
+    const toUserId = req.params.toUserId; 
+    const status = req.params.status;
+
+    const allowedStatuses = ["ignored", "interested"];
+    if (!allowedStatuses.includes(status)) {
+      return res.status(400).json({ message: "Invalid status value : "  + status });
+    }
+
+  
+      
+    const toUser = await User.findById(toUserId);
+    if (!toUser) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    
+
+      
+    const existingConnectionRequest = await ConnectionRequest.findOne({ 
+      $or : [
+        { fromUserId, toUserId },
+        { fromUserId: toUserId, toUserId: fromUserId },
+      ],
+     });
+     if(existingConnectionRequest){
+        throw new Error("Connection request already exists");
+      }
+
+
+  
+    // Create a new connection request
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
     });
 
-    module.exports = requestRouter;
+    const data = await connectionRequest.save();
+
+    return res.json({
+      message: req.user.firstName+" is "+status+" in "+ toUser.firstName,
+      data,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: "Failed to send connection request",
+      details: err.message,
+    });
+  }
+});
+
+module.exports = requestRouter;
